4925a0f0d25056f089dcb4f6f0dbab43ac33b029
dmaristany <163365757+dmaristany@users.noreply.github.com>
2024-03-14 10:36:03 -0300

    Update cachipun.py
    Soy Dominga.
    He hecho el cachipun que funciona con números, el jugador escoge un numero entre el 1 al 3, luego el computador escoge uno al azar, y se imprime si fue un empate, si gano el jugador o el computador.

--- juegos/cachipun.py
+++ juegos/cachipun.py
@@ -1,7 +1,29 @@
-def cachipun():
-    """
-    Esta función representa el juego de cachipun.
-    Debes pedir al usuario que elija piedra, papel o tijera, y luego comparar su elección con la de la computadora.
-    La computadora debe elegir una opción al azar.
-    """
+import random
+def cachipun(jugador,computador):
+    if jugador==1:
+        if computador==2:
+            print("Ganó el computador")
+        elif computador==3:
+            print("Ganó el jugador")
+        else:
+            print("Empate")
+    elif jugador==2:
+        if computador==3:
+            print("Ganó el computador")
+        elif computador==1:
+            print("Ganó el jugador")
+        else:
+            print("Empate")
+    else:
+        if computador==1:
+            print("Ganó el computador")
+        elif computador==2:
+            print("Ganó el jugador")
+        else:
+            print("Empate")
+    
     pass
+jugador=int(input("Cachipun (1 es piedra, 2 es papel, 3 es tijera) "))
+computador=random.randint(1,3)
+print("El computador escogio",computador)
+cachipun(jugador,computador)
